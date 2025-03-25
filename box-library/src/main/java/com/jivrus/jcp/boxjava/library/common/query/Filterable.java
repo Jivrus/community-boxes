baac335ca94af86868ad4ae6417eb49283842218
Gokul Nagesh <gokul@jivrus.com>
2025-03-25 15:08:54 +0530

    Removed Library Reference in Repo

--- box-library/src/main/java/com/jivrus/jcp/boxjava/library/common/query/Filterable.java
+++
@@ -1,5 +0,0 @@
-package com.jivrus.jcp.boxjava.library.common.query;
-
-public interface Filterable {
-	String buildFilter(String left, Operator operator, Object right);
-}
--- box-library/src/main/java/com/jivrus/jcp/boxjava/library/common/query/Operation.java
+++
@@ -1,9 +0,0 @@
-package com.jivrus.jcp.boxjava.library.common.query;
-
-
-//TODO - check whether this class is required
-
-public class Operation {
-	
-
-}
